6ac61d170d30f10076ae69a85b91a8944f155fe1
ewgeny_nikol <ewg240@yandex.ru>
2026-03-12 15:46:07 +0300

    объединил тесты в data-driven

--- tests/Validator.test.js
+++ tests/Validator.test.js
@@ -2,11 +2,10 @@ import Validator from '../src/Validator';
 
 describe('Validator', () => {
   test('должен возвращать false для не-строковых значений', () => {
-    expect(Validator.validateUsername(null)).toBe(false);
-    expect(Validator.validateUsername(undefined)).toBe(false);
-    expect(Validator.validateUsername(123)).toBe(false);
-    expect(Validator.validateUsername({})).toBe(false);
-    expect(Validator.validateUsername([])).toBe(false);
+    const invalidInputs = [null, undefined, 123, {}, []];
+    invalidInputs.forEach(input => {
+      expect(Validator.validateUsername(input)).toBe(false);
+    });
   });
 
   test('должен возвращать false для пустой строки', () => {
@@ -14,54 +13,49 @@ describe('Validator', () => {
   });
 
   test('должен возвращать false для строк с недопустимыми символами', () => {
-    expect(Validator.validateUsername('user@name')).toBe(false);
-    expect(Validator.validateUsername('user#name')).toBe(false);
-    expect(Validator.validateUsername('user!name')).toBe(false);
-    expect(Validator.validateUsername('пользователь')).toBe(false);
-    expect(Validator.validateUsername('user name')).toBe(false);
+    const invalidSymbols = ['user@name', 'user#name', 'user!name', 'пользователь', 'user name'];
+    invalidSymbols.forEach(name => {
+      expect(Validator.validateUsername(name)).toBe(false);
+    });
   });
 
-  test('должен возвращать false если имя начинается с цифры', () => {
-    expect(Validator.validateUsername('1username')).toBe(false);
+  test('должен возвращать false если имя начинается с недопустимых символов', () => {
+    const invalidStarts = ['1username', '_username', '-username'];
+    invalidStarts.forEach(name => {
+      expect(Validator.validateUsername(name)).toBe(false);
+    });
   });
 
-  test('должен возвращать false если имя начинается с _', () => {
-    expect(Validator.validateUsername('_username')).toBe(false);
-  });
-
-  test('должен возвращать false если имя начинается с -', () => {
-    expect(Validator.validateUsername('-username')).toBe(false);
-  });
-
-  test('должен возвращать false если имя заканчивается цифрой', () => {
-    expect(Validator.validateUsername('username1')).toBe(false);
-  });
-
-  test('должен возвращать false если имя заканчивается _', () => {
-    expect(Validator.validateUsername('username_')).toBe(false);
-  });
-
-  test('должен возвращать false если имя заканчивается -', () => {
-    expect(Validator.validateUsername('username-')).toBe(false);
+  test('должен возвращать false если имя заканчивается недопустимыми символами', () => {
+    const invalidEnds = ['username1', 'username_', 'username-'];
+    invalidEnds.forEach(name => {
+      expect(Validator.validateUsername(name)).toBe(false);
+    });
   });
 
   test('должен возвращать false если содержит более трёх цифр подряд', () => {
-    expect(Validator.validateUsername('user1234name')).toBe(false);
-    expect(Validator.validateUsername('1234user')).toBe(false);
-    expect(Validator.validateUsername('user12345')).toBe(false);
+    const invalidDigits = ['user1234name', '1234user', 'user12345'];
+    invalidDigits.forEach(name => {
+      expect(Validator.validateUsername(name)).toBe(false);
+    });
   });
 
   test('должен возвращать true для валидных имен', () => {
-    expect(Validator.validateUsername('username')).toBe(true);
-    expect(Validator.validateUsername('user_name')).toBe(true);
-    expect(Validator.validateUsername('user-name')).toBe(true);
-    expect(Validator.validateUsername('user123name')).toBe(true);
-    expect(Validator.validateUsername('u123ser')).toBe(true);
-    expect(Validator.validateUsername('a1b2c')).toBe(true);
-    expect(Validator.validateUsername('abc')).toBe(true);
-    expect(Validator.validateUsername('a-b_c')).toBe(true);
-    expect(Validator.validateUsername('john_doe')).toBe(true);
-    expect(Validator.validateUsername('john123doe')).toBe(true);
-    expect(Validator.validateUsername('j123ohn_d456oe')).toBe(true);
+    const validNames = [
+      'username',
+      'user_name',
+      'user-name',
+      'user123name',
+      'u123ser',
+      'a1b2c',
+      'abc',
+      'a-b_c',
+      'john_doe',
+      'john123doe',
+      'j123ohn_d456oe'
+    ];
+    validNames.forEach(name => {
+      expect(Validator.validateUsername(name)).toBe(true);
+    });
   });
 });
